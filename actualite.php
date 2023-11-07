22c62473d0d1a5e06ad564377d73135698770166
Anthony Laplane <anthony.laplane@gmail.com>
2023-11-07 09:44:09 +0100

    fixing missing session

--- actualite.php
+++ actualite.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__ . "/lib/config.php";
+require_once __DIR__ . "/lib/session.php";
 require_once __DIR__ . "/lib/pdo.php";
 require_once __DIR__ . "/lib/article.php";
 require_once __DIR__ . "/templates/header.php";
